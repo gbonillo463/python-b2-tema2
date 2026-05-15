45626a5f194b57fded500183c6919124aefec9bd
gbonillo463 <spamgerard4@gmail.com>
2026-05-15 17:25:43 +0200

    ej 2a3 completado

--- 2a/ej2a3.py
+++ 2a/ej2a3.py
@@ -25,10 +25,15 @@ import typing as t
 def create_and_modify_meshgrid(
     start: int, end: int, step: int
 ) -> t.Tuple[np.ndarray, np.ndarray]:
-    # Write here your code
-    pass
+    x = np.arange(start, end + 1 , step)
+    y = np.arange(start, end + 1, step)
+    X, Y = np.meshgrid(x, y)
+
+    X[0, :] = 99
+    return X, Y
+    
 
 
 # Para probar tu código, puedes usar los siguientes parámetros:
-# X, Y = create_and_modify_meshgrid(-5, 5, 1)
-# print(X)
+X, Y = create_and_modify_meshgrid(-5, 5, 1)
+print(X)
